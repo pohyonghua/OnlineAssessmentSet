6a8f73630808274b43882e564775ad5020e091b8
tyrng <ongtypc@gmail.com>
2018-11-19 21:51:36 +0800

    drop 'proggramecode' table and 'progid'

--- OnlineAssessmentSet/OnlineAssessmentModel.Designer.cs
+++ OnlineAssessmentSet/OnlineAssessmentModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\User\Documents\Visual Studio 2017\(SEM2, DEG) Projects\OnlineAssessmentSet\OnlineAssessmentSet\OnlineAssessmentModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Users\Tyrone\Github\OnlineAssessmentSet\OnlineAssessmentSet\OnlineAssessmentModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
